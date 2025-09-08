7d6b80e21ae56eac2620df312387acfa7a039a6e
diwakar1593 <diwakar200kumar@gmail.com>
2025-09-09 01:49:54 +0530

    first commit

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const MainLayout = ({children}) => {
   return (
     <>
+      <ToastContainer />
       <Navbar />
       {children}
     </>
