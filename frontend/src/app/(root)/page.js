42f47116166a321ebdd437a53777a4b0f5c92bc6
diwakar1593 <diwakar200kumar@gmail.com>
2025-09-19 20:48:28 +0530

    feat: enhance HomePage layout and add DashboardCard component; create ErrorPage component

--- frontend/src/app/(root)/page.js
+++ frontend/src/app/(root)/page.js
@@ -1,7 +1,32 @@
+import { BsCoin } from "react-icons/bs";
+
 const HomePage=()=>{
   return <>
-  <h1>Hello PBI</h1>
+  <div className="py-10 flex flex-col gap-y-4">
+    <div className="">
+      <h1 className="text-5xl font-bold">PBI</h1>
+    </div>
+    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-x-3">
+
+    {
+      Array(3).fill(null).map((cur,i)=>{
+        return <DashboardCard key={i} />
+      })
+    }
+        
+    </div>
+  </div>
   </>
 }
 
 export default HomePage
+
+const DashboardCard = () => {
+  return <div className="flex items-center justify-between border py-3">
+    <BsCoin className="text-3xl" />
+    <div className="flexflex-col gap-y-2">
+      <p>Amount</p>
+      <h1>&#8377; 45</h1>
+    </div>
+  </div>
+}
--- frontend/src/app/(root)/template.js
+++ frontend/src/app/(root)/template.js
@@ -7,6 +7,8 @@ import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import { useSelector, useDispatch } from 'react-redux';
 import { setIsToggle, SidebarSlicePath } from '@/src/redux/slice/sidebarSlice';
 import Link from 'next/link';
+import { MdDashboard } from 'react-icons/md';
+import { GiFalloutShelter } from 'react-icons/gi';
 
 const RootTemplate = ({children}) => {
 
@@ -31,10 +33,18 @@ const RootTemplate = ({children}) => {
         </div>
     }
 
-    const CustomMenu = ({link, text}) => {
+    const CustomMenu = ({link, text, Icon}) => {
         const pathname = usePathname();
         return <>
-            <MenuItem component={<Link href={link} />}> {text} </MenuItem>
+            <MenuItem
+                style={{
+                    background: pathname === link ? '#145dfb':'#ffff',
+                    color: pathname === link ? 'white':'black',
+                    borderRadius: pathname === link ? '10px':'0px'
+                }}
+                icon={<Icon className="text-2xl" />}
+                component={<Link href={link} />}> {text} 
+            </MenuItem>
         </>
     }
 
@@ -42,12 +52,12 @@ const RootTemplate = ({children}) => {
         <>
            <section className='flex item-start'>
                 <Sidebar breakPoint='lg' toggled={isToggle} onBackdropClick={() => dispatch(setIsToggle())}>
-                        <Menu className='!bg-white !min-h-screen lg:!min-h-[90vh] px-3 py-10'>
-                            <CustomMenu link={'/'} text={'Home'}/>
-                            <MenuItem> Calendar </MenuItem>
+                        <Menu className='!bg-white !min-h-screen lg:!min-h-[90vh]'>
+                            <CustomMenu link={'/'} text={'Home'} Icon={MdDashboard}/>
+                            <CustomMenu link={'/profile'} text={'Profile'} Icon={GiFalloutShelter}/>
                         </Menu>
                     </Sidebar>
-                    <main className='px-1 md:px-4'>    
+                    <main className='px-1 md:px-3 w-full'>    
                         {children}
                     </main>
            </section>
